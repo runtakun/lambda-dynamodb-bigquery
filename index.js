b412035627e2fe11b2ce227c2e7a84972e7ead65
Hiroyuki Shigeta <shigeta@glgl.org>
2015-08-07 15:12:36 +0900

    send dynamodb records to bigquery

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+var config = require('./gcpconfig');
+var gcloud = require('gcloud');
+var unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
 var _ = require('lodash');
 
-exports.handler = function(event, contenxt) {
+exports.handler = function(event, context) {
 
   var rows = _.chain(event.Records)
   .filter(function(obj) {
     return obj.eventName == 'INSERT' || obj.eventName == 'MODIFY';
   })
   .pluck('dynamodb.NewImage')
-  .map(function(el) {
-    var item = _.mapValues(el, function(obj) {
-      var val;
-      if (_.has(obj, 'S')) {
-        val = obj.S;
-      } else if (_.has(obj, 'N')) {
-        val = _.parseInt(obj.N, 10);
-      }
-      return val;
-    });
-    return item;
+  .map(function(element) {
+    return unmarshalItem(element);
   });
   console.log('rows:', JSON.stringify(rows, null, 2));
+
+  var bigquery = gcloud.bigquery({
+    projectId: config.project,
+    keyFilename: 'gcpkey.json',
+  });
+  var table = bigquery.dataset(config.dataset).table(config.table);
+
+  table.insert(rows, function(err, insertErrors) {
+    if (err) return context.done(err);
+    if (insertErrors && insertErrors.length > 0) {
+      _.forEach(insertErrors, function (insertError){
+        console.log(insertError.row);
+        _.forEach(insertError.error, function(e) {
+          console.log("%s: %s", e.reason, e.message);
+        });
+      });
+      return context.done("error");
+    }
+
+    context.done(null, "success");
+  });
 };
