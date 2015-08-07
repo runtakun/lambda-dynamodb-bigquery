89d9fe29cca7379a0435f93a2af91fe7b934beda
Hiroyuki Shigeta <shigeta@glgl.org>
2015-08-07 11:25:58 +0900

    Add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+	lambda_invoke: {
+		default: {
+			options: {
+			}
+		}
+	},
+	lambda_deploy: {
+		default: {
+			options: {
+				region: 'us-east-1'
+			},
+			function: 'dynamodb-to-bigquery'
+		}
+	},
+	lambda_package: {
+		default: {
+		}
+	}
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
