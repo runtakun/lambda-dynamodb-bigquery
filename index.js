729d884071780d8b283ee7f5cc2c826f36184d8d
Hiroyuki Shigeta <shigeta@glgl.org>
2015-08-07 14:12:24 +0900

    convert dynamodb records to bigquery rows

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+var _ = require('lodash');
 
 exports.handler = function(event, contenxt) {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  var rows = _.chain(event.Records)
+  .filter(function(obj) {
+    return obj.eventName == 'INSERT' || obj.eventName == 'MODIFY';
+  })
+  .pluck('dynamodb.NewImage')
+  .map(function(el) {
+    var item = _.mapValues(el, function(obj) {
+      var val;
+      if (_.has(obj, 'S')) {
+        val = obj.S;
+      } else if (_.has(obj, 'N')) {
+        val = _.parseInt(obj.N, 10);
+      }
+      return val;
+    });
+    return item;
+  });
+  console.log('rows:', JSON.stringify(rows, null, 2));
 };
